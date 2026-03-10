b66589eaddb13862e78bc342a415d4aa237d964b
Kio <islemmaki11@gmail.com>
2026-03-10 19:18:23 +0100

    main.C

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "include/fichiercsv.h"
+#include "include/affichage.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    Parking tab[MAX_PARKING];
+    int nbParkings = 0;
+
+    lesparkings("../parking-metropole.csv", &nbParkings, tab);
+
+    if (nbParkings > 0) {
+        afficheparking(tab[0]);
+    }
+
+    affichesparking(tab, nbParkings);
     return 0;
 }
